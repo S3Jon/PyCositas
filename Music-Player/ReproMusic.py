bdd4e2499b68dadbdc21c886ae7ffbb622b024d1
Wciv <sanchezeguiajon@gmail.com>
2022-05-24 23:00:36 +0200

    v2 WIP

--- Music-Player/ReproMusic.py
+++ Music-Player/ReproMusic.py
@@ -3,29 +3,47 @@ import tkinter as tk
 from tkinter import filedialog
 import os
 import pygame
+import fnmatch
 
-class ReproMusic:
-    def __init__(self, root):
-        root.title("music player")
-        root.geometry('500x500')
-        root.resizable(0, 0)
-        pygame.init()
-        mydir = r"C:\Users\jonsa\Desktop\x"
-        songs = filter(lambda x: x.endswith('.mp3'), os.listdir(mydir))
-        play_button = Button(root, text="Play", command=lambda: self.play(currentsong))
-        play_button.grid(row=5, column=0)
-    
-    def play(self, currentsong):
-        self.currentsong = currentsong
-        print(currentsong)
-        pygame.mixer.init()
-        pygame.mixer.music.load(str(currentsong))
-        pygame.mixer.music.play()
-        while (pygame.mixer.get_busy() ):
-            time.sleep(400)
+#path a carpeta + file filter
+mpath = "C:/Users/jonsa/Desktop/x"
+pattern = "*.mp3"
 
+#crear ventana
+ventana = tk.Tk()
+ventana.title("ReproMusic")
+ventana.geometry("300x400")
+ventana.config(bg = 'black')
+
+#listarmusica
+
+listBox = tk.Listbox(ventana, fg="green", bg="black", width=100)
+listBox.pack(padx = 15, pady = 15)
+
+label = tk.Label(ventana, text = '', bg = "black", fg = "yellow")
+label.pack(pady = 15)
+
+#botones
+
+top = tk.Frame(ventana, bg = "black")
+top.pack(padx = 10, pady = 5, anchor = 'center') #anchor para poner todos los botones en hz
+
+prevButton = tk.Button(ventana, text = "Anterior")
+prevButton.pack(pady = 15, in_ = top, side = 'left')
+
+stopButton = tk.Button(ventana, text = "Play")
+stopButton.pack(pady = 15, in_ = top, side = "left")
+
+stopButton = tk.Button(ventana, text = "Stop")
+stopButton.pack(pady = 15, in_ = top, side = "left")
+
+stopButton = tk.Button(ventana, text = "Next")
+stopButton.pack(pady = 15, in_ = top, side = "left")
+
+#filtrosfiles
+
+for root, dirs, files in os.walk(mpath):
+    for filename in fnmatch.filter(files, pattern):
+        listBox.insert('end', filename)
         
-        
-root = Tk()
-ReproMusic(root)
-mainloop()
+ventana.mainloop()
