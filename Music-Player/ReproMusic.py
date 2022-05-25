19ea498fa1cf663c69449a3c84b78058f97431bd
Wciv <sanchezeguiajon@gmail.com>
2022-05-25 02:12:54 +0200

    C'est fini

--- Music-Player/ReproMusic.py
+++ Music-Player/ReproMusic.py
@@ -26,6 +26,36 @@ def select():
 def stop():
     mixer.music.stop()
     listBox.select_clear('active')
+    
+def pnext():
+    next_song = listBox.curselection()
+    next_song = next_song[0] + 1
+    next_song_name = listBox.get(next_song)
+    label.config(text = next_song_name)
+    mixer.music.load(mpath + "\\" + next_song_name)
+    mixer.music.play()
+    listBox.select_clear(0, 'end')
+    listBox.activate(next_song)
+    listBox.select_set(next_song)
+
+def pprev():
+    next_song = listBox.curselection()
+    next_song = next_song[0] - 1
+    next_song_name = listBox.get(next_song)
+    label.config(text = next_song_name)
+    mixer.music.load(mpath + "\\" + next_song_name)
+    mixer.music.play()
+    listBox.select_clear(0, 'end')
+    listBox.activate(next_song)
+    listBox.select_set(next_song)
+
+def pause():
+    if pausButton["text"] == "Pause":
+        mixer.music.pause()
+        pausButton["text"] = "Play"
+    else:
+        mixer.music.unpause()
+        pausButton["text"] = "Pause"
 
 #listarmusica
 
@@ -47,19 +77,19 @@ stop_img = tk.PhotoImage(file = "Stop.png")
 top = tk.Frame(ventana, bg = "black")
 top.pack(padx = 10, pady = 5, anchor = 'center') #anchor para poner todos los botones en hz
 
-prevButton = tk.Button(ventana, bg = "green", text = "", image= back_img)
+prevButton = tk.Button(ventana, bg = "green", text = "", image= back_img, command=pprev)
 prevButton.pack(pady = 15, in_ = top, side = 'left')
 
 playButton = tk.Button(ventana, bg = "green", text = "", image= play_img, command= select)
 playButton.pack(pady = 15, in_ = top, side = "left")
 
-pausButton = tk.Button(ventana, bg = "green", text = "", image= paus_img)
+pausButton = tk.Button(ventana, bg = "green", text = "Pause", image= paus_img, command=pause)
 pausButton.pack(pady = 15, in_ = top, side = "left")
 
 stopButton = tk.Button(ventana, bg = "green", text = "", image= stop_img, command=stop)
 stopButton.pack(pady = 15, in_ = top, side = "left")
 
-nextButton = tk.Button(ventana, bg = "green", text = "", image= next_img)
+nextButton = tk.Button(ventana, bg = "green", text = "", image= next_img, command=pnext)
 nextButton.pack(pady = 15, in_ = top, side = "left")
 
 #filtrosfiles + poner musica en lista
